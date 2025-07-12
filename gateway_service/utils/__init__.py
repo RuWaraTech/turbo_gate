ca036e0070fdf323068b5dc722180f286bbe538b
Farai Wande <faraiwande@gmail.com>
2025-07-12 15:50:46 +0100

    refactor:  Moves utility function imports into `__init__.py` to simplify consumption of core utils.

---
+++ gateway_service/utils/__init__.py
@@ -0,0 +1,8 @@
+from gateway_service.utils.utils import setup_logging, get_redis_client, generate_request_id
+
+
+__all__ = [
+    'setup_logging',
+    'get_redis_client',
+    'generate_request_id'
+]
